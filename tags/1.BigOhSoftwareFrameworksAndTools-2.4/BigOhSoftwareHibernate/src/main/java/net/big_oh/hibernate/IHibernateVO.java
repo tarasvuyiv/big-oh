c3f0535f0ba606777388d41e08c4d0df2d1c641c
DaveWingate <DaveWingate@f7115b3b-593d-2812-1388-843e33ee7d22>
2010-08-08 21:19:19 +0000

    retry 2.4 build

--- tags/1.BigOhSoftwareFrameworksAndTools-2.4/BigOhSoftwareHibernate/src/main/java/net/big_oh/hibernate/IHibernateVO.java
+++
@@ -1,9 +0,0 @@
-package net.big_oh.hibernate;
-
-// TODO DSW Use generic type parameter for key class?
-//
-// // TODO DSW Rename to IHibernateDomainObject?
-public interface IHibernateVO
-{
-	public Object[] getBusinessKeys();
-}
