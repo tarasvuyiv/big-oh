327b0b3ef47c9a9fa6bc63425a639cb95bf57694
DaveWingate <DaveWingate@f7115b3b-593d-2812-1388-843e33ee7d22>
2010-08-15 08:19:43 +0000

    Declare modifier methods to throw UnsupportedOperationException for read only vertices.

--- 1.BigOhSoftwareFrameworksAndTools/BigOhSoftwareDataStructuresAndAlgorithms/src/main/java/net/big_oh/datastructures/graph/Vertex.java
+++ 1.BigOhSoftwareFrameworksAndTools/BigOhSoftwareDataStructuresAndAlgorithms/src/main/java/net/big_oh/datastructures/graph/Vertex.java
@@ -1,29 +1,29 @@
 package net.big_oh.datastructures.graph;
 
 /*
-Copyright (c) 2009 Dave Wingate dba Big-Oh Software (www.big-oh.net)
+ Copyright (c) 2009 Dave Wingate dba Big-Oh Software (www.big-oh.net)
 
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following
-conditions:
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
 
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-*/
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 /**
  * Provides a base level of abstraction graph algorithms. The underlying
@@ -40,8 +40,11 @@ public interface Vertex
 	 * Sets a unique identifier for this vertex.
 	 * 
 	 * @param id
+	 * 
+	 * @throws UnsupportedOperationException
+	 *             is the vertex is read only
 	 */
-	public void setIdentifier(Object id);
+	public void setIdentifier(Object id) throws UnsupportedOperationException;
 
 	/**
 	 * @return the unique identifier for this vertex
@@ -52,8 +55,11 @@ public interface Vertex
 	 * Sets the data pay load for this vertex.
 	 * 
 	 * @param value
+	 * 
+	 * @throws UnsupportedOperationException
+	 *             is the vertex is read only
 	 */
-	public void setValue(Object value);
+	public void setValue(Object value) throws UnsupportedOperationException;
 
 	/**
 	 * @return the data pay load for this vertex
